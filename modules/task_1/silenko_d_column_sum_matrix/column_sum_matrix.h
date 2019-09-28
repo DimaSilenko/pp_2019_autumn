886ad9c50f3602e8212954ce9a2c25f888f9f5d3
DimaSilenko <43129870+DimaSilenko@users.noreply.github.com>
2019-09-28 21:42:45 +0300

    Comments repair

--- modules/task_1/silenko_d_column_sum_matrix/column_sum_matrix.h
+++ modules/task_1/silenko_d_column_sum_matrix/column_sum_matrix.h
@@ -1,11 +1,12 @@
 // Copyright 2019 Silenko Dmitrii
-#ifndef MODULES_TASK_1_Silenko_D_COLUMN_SUM_MATRIX_COLUMN_SUM_MATRIX_H_
-#define MODULES_TASK_1_Silenko_D_COLUMN_SUM_MATRIX_COLUMN_SUM_MATRIX_H_
+#ifndef MODULES_TASK_1_SILENKO_D_COLUMN_SUM_MATRIX_COLUMN_SUM_MATRIX_H_
+#define MODULES_TASK_1_SILENKO_D_COLUMN_SUM_MATRIX_COLUMN_SUM_MATRIX_H_
+
 
 #include <vector>
 
-std::vector <std::vector <int>> getRandomMatrixE(int n, int m); // Only 1
-std::vector <std::vector <int>> getRandomMatrixO(int n, int m); // Eq rows
+std::vector <std::vector <int>> getRandomMatrixE(int n, int m); //  Only 1
+std::vector <std::vector <int>> getRandomMatrixO(int n, int m); //  Eq rows
 
 std::vector <int> ColumnSumMatrix(const std::vector <std::vector <int>> &a, int n, int m);
 
